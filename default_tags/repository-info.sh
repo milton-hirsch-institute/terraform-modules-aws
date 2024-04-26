fc6a25f4aa01fcca73655ee3fb7167a431917bbd
Rafe Kaplan <766471+slobberchops@users.noreply.github.com>
2024-04-26 15:34:01 +0200

    Added error handlign into default_tags module

--- default_tags/repository-info.sh
+++ default_tags/repository-info.sh
@@ -1,12 +1,12 @@
 # Copyright 2024 The Milton Hirsch Institute, B.V.
 # SPDX-License-Identifier: Apache-2.0
 
-template_root=`realpath $1`
+template_root=`realpath $1` || exit 1
 
-name=`gh repo view --json name -q .name`
-owner=`gh repo view --json owner -q .owner.login`
-git_root=`git rev-parse --show-toplevel`
-path=${template_root#"$git_root/"}
+name=`gh repo view --json name -q .name` || exit 1
+owner=`gh repo view --json owner -q .owner.login` || exit 1
+git_root=`git rev-parse --show-toplevel` || exit 1
+path=${template_root#"$git_root/"} || exit 1
 
 echo """
 {
